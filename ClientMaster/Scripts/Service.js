5ac2b101b67322bfedf5a120b2c11cc61597b6d2
Vinit Suryarao <suryarao.vinit@hotmail.com>
2022-11-26 16:38:21 +0530

    First Commit

---
+++ ClientMaster/Scripts/Service.js
@@ -0,0 +1,26 @@
+﻿function getAllClients() {
+    $.ajax({
+        url: "Service/ClientService.svc/GetAllClients",
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            clients = result;
+            bindClientListToTable(clients);
+        }
+    });
+}
+
+function searchClient() {
+    var id = $("#searchId").val();
+    $.ajax({
+        url: "Service/ClientService.svc/Client/"+id,
+        type: "GET",
+        dataType: "json",
+        success: function (result) {
+            console.log(result);
+            client = result;
+            bindClientListToTable(client);
+        }
+    });
+}
+
---
+++ ClientMaster/Service/ClientService.svc.cs
@@ -0,0 +1,30 @@
+﻿using ClientMaster.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ClientMaster.Service
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class ClientService
+    {
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<ClientModel> GetAllClients() // Get All Record
+        {
+            return ClientData.Data;
+        }
+
+        [OperationContract, WebGet(ResponseFormat = WebMessageFormat.Json, UriTemplate = "Client/{Id}")]
+        public ClientModel GetSingleClient(string Id) // Get Single Record With Cust URL
+        {
+            return ClientData.Data.Find(x => x.Id == Convert.ToInt32(Id));
+        }
+
+    }
+}
